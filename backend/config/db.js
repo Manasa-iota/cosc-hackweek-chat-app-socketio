ed48153bb6c021ca41d41b24de8582b87e06e5ed
Manasa-iota <nagarammanasa235@gmail.com>
2025-06-22 20:17:29 +0530

    backend inital setup

---
+++ backend/config/db.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose"
+
+export const connectDB = async () => {
+  await mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+}
